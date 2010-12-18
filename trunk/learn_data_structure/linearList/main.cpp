6118122991acbbc1b8e48d4631fe08d306ad9860
vrcats <vrcats@69c8e5a0-5008-11de-9cb9-2f00d435f02c>
2010-12-18 11:58:22 +0000

    structure modified, started linear list

---
+++ trunk/learn_data_structure/linearList/main.cpp
@@ -0,0 +1,20 @@
+#include "LinearList.h"
+#include <stdio.h>
+
+using namespace vrcats.com;
+/*
+    The purpose of this file is to test the class
+*/
+int main()
+{
+    //creation
+    //destruction
+    //resize
+    //clear
+    //length
+    //getItem
+    //getIndex
+    //insert
+    //del
+    //sort
+}
