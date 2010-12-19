7867e6b46820794ff1e2814f8085bc7185e47e48
vrcats <vrcats@69c8e5a0-5008-11de-9cb9-2f00d435f02c>
2010-12-19 04:28:28 +0000

    now working basically

--- trunk/learn_data_structure/linearList/main.cpp
+++ trunk/learn_data_structure/linearList/main.cpp
@@ -1,20 +1,71 @@
+#define DEBUG
 #include "LinearList.h"
-#include <stdio.h>
-
-using namespace vrcats.com;
+using namespace vrcats;
 /*
     The purpose of this file is to test the class
 */
 int main()
 {
     //creation
-    //destruction
-    //resize
-    //clear
-    //length
-    //getItem
-    //getIndex
+    LinearList<int> list5(5);
+    LinearList<long> list;
     //insert
+    std::cout<<"insert 3"<<std::endl;
+    list.insert(3);
+    list.print();
+    std::cout<<"insert 2"<<std::endl;
+    list.insert(2);
+    list.print();
+    std::cout<<"insert 1"<<std::endl;
+    list.insert(1);
+    list.print();
+    std::cout<<"insert 4, 2"<<std::endl;
+    list.insert(4,2);
+    list.print();
+    std::cout<<"insert 5, 0"<<std::endl;
+    list.insert(5,0);
+    list.print();
+    std::cout<<"insert 6, 1"<<std::endl;
+    list.insert(6,0);
+    list.print();
+    std::cout<<"insert 7, 20"<<std::endl;
+    list.insert(7, 20);
+    list.print();
+    //find
     //del
+    std::cout<<"del 3"<<std::endl;
+    list.del(3);
+    list.print();
+    std::cout<<"del 0"<<std::endl;
+    list.del(0);
+    list.print();
+    std::cout<<"del 7"<<std::endl;
+    list.del(7);
+    list.print();
+    std::cout<<"del 1"<<std::endl;
+    list.del(1);
+    list.print();
+    std::cout<<"del 3"<<std::endl;
+    list.del(3);
+    list.print();
     //sort
+    //not implemented yet
+    //length
+    std::cout<<"Length is: "<<list.length();
+    //[]
+    list.insert(33, 2);
+    list.insert(66);
+    list.print();
+    std::cout<<"item 1,2,3 are: "<<list[1]<<", "<<list[2]<<", "<<list[3]<<std::endl;
+    //resize
+    std::cout<<"resize 6"<<std::endl;
+    list.resize(6);
+    list.print();
+    std::cout<<"resize 2"<<std::endl;
+    list.resize(2);
+    list.print();
+    //clear
+    list.clear();
+    list.print();
+    return 0;
 }
